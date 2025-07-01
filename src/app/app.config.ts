ce0a790de9d0930488f04d9807170999a979c5b8
Presidio <presidio@Presidios-MacBook-Pro.local>
2025-07-01 09:39:52 +0530

    Frontend Angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { UserService } from './services/UserService';
+import { provideHttpClient } from '@angular/common/http';
+
+import { ToastService } from './services/ToastService';
+import { CoachService } from './services/CoachService';
+import { WorkoutLogService } from './services/WorkoutLogService';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    UserService,
+    ToastService,
+    CoachService,
+    WorkoutLogService,
+  ],
 };
